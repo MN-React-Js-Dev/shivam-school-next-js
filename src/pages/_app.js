efe5e3a3e04e4dc88bdc42638cd5cd68317e6d4f
mn-react-js-dev <mayur.nandwalker.5057@gmail.com>
2023-07-22 13:10:46 +0530

    new chnages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,11 @@ import "../assets/css/bootstrap.min.css";
 import "../assets/css/style.css";
 
 import { Fragment } from "react";
- 
+
 export default function App({ Component, pageProps }) {
   return (
     <Fragment>
- 
       <Component {...pageProps} />
-     </Fragment>
+    </Fragment>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
+
 export default function Document() {
 
   return (
@@ -9,10 +10,10 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com/" />
       <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
 
+      <link rel="shortcut icon" href="/public/favicon.ico" />
       <link
         href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@600&family=Lobster+Two:wght@700&display=swap"
         rel="stylesheet" />
-
 
       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
